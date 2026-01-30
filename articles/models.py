6246e3e76d9797d5014d5c6f61851c7aeac8f70e
georgebarrett <georgeaustinbarrett@gmail.com>
2026-01-30 15:51:09 +0000

    add Article model with the essential data fields for articles. linked articles with users through AUTH_USER_MODEL. add draft and published statuses using text choices. include timestamps. apply default ordering and index on publish date

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Article(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
+    body = models.TextField()
+    image = models.ImageField(upload_to='articles/')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length = 2,
+        choices = Status.choices,
+        default = Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
