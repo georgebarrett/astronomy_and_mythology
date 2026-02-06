de5015a8de1925be472aa207d3a97949babeaea9
georgebarrett <georgeaustinbarrett@gmail.com>
2026-02-06 17:08:48 +0000

    prepolated_fields syntax correction. tuple expected. add an image thumbnail for articles about the cosmos

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Article
 
 @admin.register(Article)
@@ -6,7 +7,13 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status', 'image_preview']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
-    prepopulated_fields = {'slug': { 'title', }}
+    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+
+    @admin.display(description='Image')
+    def image_preview(self, object):
+        if object.image:
+            return format_html('<img src="{}" style="height: 50px;" />', object.image.url)
+        return '-'
